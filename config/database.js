d8fc89a8eb9fe6bbb86704f204ba4810be79a7d6
anhld <wolfwithcode@gmail.com>
2021-04-09 10:43:28 +0700

    merge

--- config/database.js
+++ config/database.js
@@ -2,17 +2,30 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'sr0BZonRScOT5hL1E6yNCyg9YvX4bWpw'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST', 'jobsrabbitdev.7d1ak.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'jobsRabbitDev'),
+        username: env('DATABASE_USERNAME', 'jobsrabbit'),
+        password: env('DATABASE_PASSWORD', 'Job@123!'),
       },
-      options: {}
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
 });
+
+
+// mongodb+srv://
+// jobsrabbit
+// :
+// Job%40123!
+// @
+// jobsrabbitdev.7d1ak.mongodb.net
+// /
+// myFirstDatabase
+// ?retryWrites=true&w=majority
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'a522418e-4c1c-4043-90cb-aae1c6e7b70d'
+  jwtSecret: process.env.JWT_SECRET || 'de26b78a-f53d-4632-8f56-b704450878ea'
 };
