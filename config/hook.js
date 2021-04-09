881dd3f7f80623595f31e4512603fa695c31599d
anhld <wolfwithcode@gmail.com>
2021-04-09 10:02:13 +0700

    merge

---
+++ config/hook.js
@@ -0,0 +1,12 @@
+module.exports = {
+    settings: {
+      // ...
+      // algolia: {
+      //   enabled: true,
+      //   applicationId: 'NRJXAFBP4P',
+      //   apiKey: '3c83a143e5fec8411fcb0b48cede9323',
+      //   debug: true,              // default: false
+      //   // prefix: 'my_own_prefix'   // default: Strapi environment (strapi.config.environment)
+      // },
+    }
+  };
