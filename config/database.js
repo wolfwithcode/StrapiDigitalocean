6d3fd6e021fb632f94b041230796c28e61b26693
anhld <wolfwithcode@gmail.com>
2021-04-09 10:47:32 +0700

    revert to postgre

--- config/database.js
+++ config/database.js
@@ -2,30 +2,17 @@ module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'bookshelf',
       settings: {
-        host: env('DATABASE_HOST', 'jobsrabbitdev.7d1ak.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'jobsRabbitDev'),
-        username: env('DATABASE_USERNAME', 'jobsrabbit'),
-        password: env('DATABASE_PASSWORD', 'Job@123!'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'sr0BZonRScOT5hL1E6yNCyg9YvX4bWpw'),
+        ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+      options: {}
     },
   },
 });
-
-
-// mongodb+srv://
-// jobsrabbit
-// :
-// Job%40123!
-// @
-// jobsrabbitdev.7d1ak.mongodb.net
-// /
-// myFirstDatabase
-// ?retryWrites=true&w=majority
